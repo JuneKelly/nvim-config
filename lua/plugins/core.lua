5a9048183923d8423e493eef94b68b38fa89b7ff
June Kelly <june@cnjctr.com>
2024-02-18 15:28:15 +0000

    Fix border color

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,9 +1,20 @@
 -- core plugin config
+
 return {
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "catppuccin",
+      colorscheme = function()
+        local tokyonight = require("tokyonight")
+        tokyonight.setup({
+          style = "moon",
+          on_colors = function(c)
+            c.border = c.fg_gutter
+          end,
+        })
+        return tokyonight.load()
+      end,
     },
   },
   {
