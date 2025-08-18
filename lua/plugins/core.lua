ad8b004ff8ceb07bc8e3f55bc4be30dccccb9752
June Kelly <june@getsona.com>
2025-08-18 11:14:29 +0100

    Switch to catppuccin theme

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,17 +4,17 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "catppuccin",
-      colorscheme = function()
-        local tokyonight = require("tokyonight")
-        tokyonight.setup({
-          style = "moon",
-          on_colors = function(c)
-            c.border = c.fg_gutter
-          end,
-        })
-        return tokyonight.load()
-      end,
+      colorscheme = "catppuccin",
+      -- colorscheme = function()
+      --   local tokyonight = require("tokyonight")
+      --   tokyonight.setup({
+      --     style = "moon",
+      --     on_colors = function(c)
+      --       c.border = c.fg_gutter
+      --     end,
+      --   })
+      --   return tokyonight.load()
+      -- end,
     },
   },
   {
