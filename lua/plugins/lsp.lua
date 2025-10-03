12ccbd330d1ce16758f4571dd420c7aabc3686ec
June Kelly <june@getsona.com>
2025-10-03 11:03:54 +0100

    Disable nextls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "nextls", -- Install Next LS through Mason
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        -- Disable the default ElixirLS
-        elixirls = {
-          mason = false, -- Don't install via Mason
-          enabled = false, -- Disable the server
-        },
-        -- Enable Next LS
-        nextls = {
-          mason = true, -- Install via Mason
-          -- Optional settings for Next LS
-          init_options = {
-            experimental = {
-              completions = {
-                enable = true, -- Enable completions
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "mason-org/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "nextls", -- Install Next LS through Mason
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       -- Disable the default ElixirLS
+  --       elixirls = {
+  --         mason = false, -- Don't install via Mason
+  --         enabled = false, -- Disable the server
+  --       },
+  --       -- Enable Next LS
+  --       nextls = {
+  --         mason = true, -- Install via Mason
+  --         -- Optional settings for Next LS
+  --         init_options = {
+  --           experimental = {
+  --             completions = {
+  --               enable = true, -- Enable completions
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
